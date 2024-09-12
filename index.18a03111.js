260449fe0e34f40ddcf5430ce87bea1c3974aca7
Hryniuk_Taras <gritaras484@gmail.com>
2024-09-12 17:50:41 +0300

    make build

---
+++ index.18a03111.js
@@ -0,0 +1,2 @@
+!function e(n,t){var r=document.createElement("ul");for(var a in t){var c=document.createElement("li");c.textContent=a,r.append(c),Object.keys(t[a]).length>0&&e(c,t[a])}n.append(r)}(document.querySelector("#tree"),{Drink:{Wine:{},Schnaps:{}},Fruit:{Red:{Cherry:{},Strawberry:{}},Yellow:{Banana:{},Pineapple:{}}}});
+//# sourceMappingURL=index.18a03111.js.map
